74203cfb84e6d4c6d056308e2cc312511a04f80f
IoanaGoroneanu <72194327+IoanaGoroneanu@users.noreply.github.com>
2022-01-31 13:24:31 +0200

    Add files via upload

---
+++ app/src/Components/AddShipView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import AddShip from './AddShip';
+import Sidebar from './Sidebar/Sidebar';
+
+function AddShipView() {
+  return(
+      <>
+      <Sidebar/>
+      <AddShip/>
+      </>
+  )
+}
+
+export default AddShipView;
