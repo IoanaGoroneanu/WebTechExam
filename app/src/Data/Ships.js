91ea8ea5c956cc7d7ac7f92566a8d731e327a3c4
IoanaGoroneanu <72194327+IoanaGoroneanu@users.noreply.github.com>
2022-01-31 13:27:04 +0200

    Add files via upload

---
+++ app/src/Data/Ships.js
@@ -0,0 +1,17 @@
+export const shipsData = [
+    {
+        name: "Lorelai",
+        displacement: "New York",
+        crewmembers: "John Smith, Mike Dane, Willow Brown"
+    },
+    {
+        name: "Lali",
+        displacement: "New York",
+        crewmembers: "Dwight Smith, Jim Scott, Andy Fox"
+    },
+    {
+        name: "TGX45",
+        displacement: "New York",
+        crewmembers: "Angela Martinez, Izabella Gonzales, Paul Carrey"
+    }
+]
